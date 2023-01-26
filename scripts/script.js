0fdf821c80ead443dff7f0ac8fa2824f79042cb2
MhmdSheref <melezaly@gmail.com>
2023-01-26 10:38:23 +0200

    added calculator function and interface, havent linked them yet

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,37 @@
+let form = document.forms[0];
+
+form.addEventListener("submit", saveToLocal);
+
 if (!localStorage.getItem("username")) {
     document.querySelector(".form-container").style.display= "flex";
 } else {
-    username= localStorage.getItem("username")
-    dob= localStorage.getItem("dob")
-    theme= localStorage.getItem("theme")
+    username= localStorage.getItem("username");
+    dob= localStorage.getItem("dob");
+    theme= localStorage.getItem("theme");
 }
 
-let form = document.forms[0];
+let mathExp = "5*5=";
+let mathAns = "15";
+let mathExpAns;
 
-form.addEventListener("submit", saveToLocal)
+if (mathExp.slice(-1) === "=") {
+    //this is an equation
+     mathExpAns = mathExp + "==" + mathAns;
+} else {
+    //this is an inequality
+     mathExpAns = mathExp;
+}
+
+if (eval(mathExpAns)){
+    //correct answer
+    console.log("correct answer");
+} else {
+    //wrong answer
+    console.log("wrong answer");
+}
+
+
+
 
 function saveToLocal () {
     localStorage.setItem("username", form.username.value);
@@ -16,8 +39,9 @@ function saveToLocal () {
     localStorage.setItem("theme", form.theme.value);
 }
 
-function calcAge () {
-    let birthDate = new Date(dob);
+function calcAge (date) {
+    let birthDate = new Date(date);
     let currentDate = new Date();
-    let age= Math.floor((currentDate - birthDate) / 31556952000);
+    return Math.floor((currentDate - birthDate) / 31556952000);
 }
+
